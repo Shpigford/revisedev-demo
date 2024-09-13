7ed4c1db92b11c13ba4195fcd43083ce2cdb9183
Josh Pigford <josh@joshpigford.com>
2024-09-13 17:39:13 -0500

    Init

---
+++ app.rb
@@ -0,0 +1,29 @@
+arequire 'sentry-ruby'
+
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.traces_sample_rate = 1.0 # Capture 100% of transactions for testing
+  config.environment = 'development'
+  config.release = '0.1.0'
+  config.debug = true # Enable debug mode for testing
+end
+
+def divide_by_zero
+  1 / 0
+end
+
+def main
+  puts "Starting the app..."
+  Sentry.capture_message("App started")
+
+  begin
+    divide_by_zero
+  rescue ZeroDivisionError => e
+    Sentry.capture_exception(e)
+    puts "Caught an error: #{e.message}"
+  end
+
+  puts "App finished."
+end
+
+main
